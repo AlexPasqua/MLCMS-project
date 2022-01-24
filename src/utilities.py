1a92954248296925eea6b27f06163c71519633b4
AlexPasqua <alexpasquali98@gmail.com>
2022-01-24 10:28:19 +0100

    Made reading data more elastic
    Allowing to pass a dataframe that then gets returns

--- src/utilities.py
+++ src/utilities.py
@@ -1,36 +1,31 @@
 import pandas as pd
 import warnings
-import copy
+from typing import Union
 
 
-def read_data(path):
+def read_data(path: Union[pd.DataFrame, str]) -> pd.DataFrame:
     """
     Reads the data form the given file path
-    :param path: path of the file containing the data
+    :param path: path of the file containing the data or a pandas Dataframe. In the last case, the func simply returns this parameter
     :return: a pandas dataframe containing the data
     """
+    # if path is already a dataframe, return it (so the function is callable without checking if you have a path or the dataframe itself already)
+    if isinstance(path, pd.DataFrame):
+        return path
+    # otherwise read the data and return the dataframe
     col_names = ["ID", "FRAME", "X", "Y", "Z"]
     data = pd.read_csv(path, sep=" ", header=None, names=col_names)
     return data
 
 
-def create_complete_dataframe(original_df: pd.DataFrame = None, original_data_path: str = None):
+def create_complete_dataframe(original_data: Union[pd.DataFrame, str]) -> pd.DataFrame:
     """
     Creates a complete dataframe containing extra - derived - data, e.g. speed and mean spacing for each pedestrian
-    :param original_df: (optional) original data as a pandas dataframe, if None, read using func "read_data"
-    :param original_data_path: (optional) path to a file containing the data. If original_df is None, this must be not None
+    :param original_data: original data as a pandas dataframe or path to the file containing the data
     :return: a pandas dataframe containing the original data plus some derived data
     """
-    # check on attributes
-    if original_df is None and original_data_path is None:
-        raise AttributeError("One attribute between original_df and original_data_path must be provided, got None and None")
-    if original_df is not None and original_data_path is not None:
-        warnings.warn("Both original_df and original_data_path are not None. original_data_path is going to be ignored", RuntimeWarning)
-
-    # at this point, only one between original_df and original_data_path is not None
-    if original_df is None:
-        original_df = read_data(original_data_path)
-    extended_df = original_df
+    # if original_data is a path, read the data; if it's a dataframe, simply return the dataframe itself
+    extended_df = read_data(original_data)
 
     # add speed to the data: frame rate of 16Hz, compute space between the current position and the next one and divide by 1/16
     pass
