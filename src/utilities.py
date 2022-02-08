a8f52e4b705e532cd76efac54bb3c37c52f8b585
AlexPasqua <alexpasquali98@gmail.com>
2022-02-08 13:51:17 +0100

    Fixed function in task4.ipynb

--- src/utilities.py
+++ src/utilities.py
@@ -90,6 +90,7 @@ def plot_fd_and_original(data_path: str, plot_title: str = "", fd_epochs: int = 
     plt.show()
     return model
 
+
 def plot_fd_and_speeds(data_path: str, plot_title: str = "", fd_epochs: int = 50, nn_epochs: int = 50, hidden_dims: Tuple[int] = (3,),
                        hidden_activation_func: str = "sigmoid", dropout = -1, training_plots: bool = True, verbose=1):
     """
@@ -150,6 +151,162 @@ def plot_fd_and_speeds(data_path: str, plot_title: str = "", fd_epochs: int = 50
     ax.set_ylabel("Speed")
     fig.suptitle(plot_title)
     plt.show()
+
+
+def get_all_result_data(results):
+    tr_mean = []
+    tr_std = []
+    val_mean = []
+    val_std = []
+    test_mean = []
+    test_std = []
+    for key in results.keys():
+        tr_mean.append(results[key]['tr'][0])
+        tr_std.append(results[key]['tr'][1])
+        val_mean.append(results[key]['val'][0])
+        val_std.append(results[key]['val'][1])
+        test_mean.append(results[key]['test'][0])
+        test_std.append(results[key]['test'][1])
+    return tr_mean, tr_std, val_mean, val_std, test_mean, test_std
+
+
+def plot_results(results, tr_mean, tr_std, val_mean, val_std, test_mean, test_std, plot_val=False, title=""):
+    fig, ax = plt.subplots()
+    ax.fill_between(range(len(tr_std)), [tr_mean[i] + tr_std[i] for i in range(len(tr_std))], [tr_mean[i] - tr_std[i] for i in range(len(tr_std))],
+                    alpha=0.2, color='orange')
+    ax.plot(tr_mean, label='training_loss', c='orange')
+    ax.scatter(range(len(tr_mean)), tr_mean, c='orange')
+
+    if plot_val:
+        ax.fill_between(range(len(val_std)), [val_mean[i] + val_std[i] for i in range(len(val_std))],
+                        [val_mean[i] - val_std[i] for i in range(len(val_std))], alpha=0.2)
+        ax.plot(val_mean, label='validation_loss')
+        ax.scatter(range(len(val_mean)), val_mean, c='blue')
+
+    ax.fill_between(range(len(test_std)), [test_mean[i] + test_std[i] for i in range(len(test_std))],
+                    [test_mean[i] - test_std[i] for i in range(len(test_std))], alpha=0.2, color='red')
+    ax.plot(test_mean, label='testing_loss', c='red')
+    ax.scatter(range(len(test_mean)), test_mean, c='red')
+    plt.legend()
+    ax.set_ylabel('MSE')
+    ax.set_xlabel('model conf')
+    ax.set_xticks(range(len(results.keys())), labels=results.keys())
+    plt.title(title)
+    plt.show()
+
+
+def _get_data_for_train_both_models(base_path, task_data, train: bool):
+    """
+    Internal usage function, called from 'train_both_models' to get the training/testing data
+    :param base_path:
+    :param task_data:
+    :param train:
+    :return:
+    """
+    X_t, y_t = None, None
+    fd_x_t = None
+    for data in task_data:
+        path = base_path + f"train_{data}_data"
+        try:
+            f = open(path)
+        except IOError:
+            create_and_save_training_testing_data(data, base_path)
+
+        X_train, y_train, X_test, y_test = read_train_test(data, base_path)
+        if train:
+            X = X_train
+            y = y_train
+        else:
+            X = X_test
+            y = y_test
+        fd_x = X[:, 0].reshape(-1, 1)
+
+        if X_t is None:
+            X_t = X
+            y_t = y
+            fd_x_t = fd_x
+        else:
+            X_t = np.concatenate((X_t, X), axis=0)
+            y_t = np.concatenate((y_t, y), axis=0)
+            fd_x_t = np.concatenate((fd_x_t, fd_x), axis=0)
+
+    X, y, fd_x = X_t, y_t, fd_x_t
+    return X, y, fd_x
+
+
+def train_both_models(task_train, task_test):
+    base_path = "../data/training_data/"
+    X_train, y_train, fd_x_train = _get_data_for_train_both_models(base_path=base_path, task_data=task_train, train=True)
+    X_test, y_test, fd_x_test = _get_data_for_train_both_models(base_path=base_path, task_data=task_test, train=False)
+
+    # X_t, y_t = None, None
+    # fd_x_t = None
+    # for train_data in task_train:
+    #     training_path = base_path + f"train_{train_data}_data"
+    #     try:
+    #         f = open(training_path)
+    #     except IOError:
+    #         create_and_save_training_testing_data(train_data, base_path)
+    #
+    #     X_train, y_train, _, _ = read_train_test(train_data, base_path)
+    #     fd_x_train = X_train[:, 0].reshape(-1, 1)
+    #
+    #     if X_t is None:
+    #         X_t = X_train
+    #         y_t = y_train
+    #         fd_x_t = fd_x_train
+    #     else:
+    #         X_t = np.concatenate((X_t, X_train), axis=0)
+    #         y_t = np.concatenate((y_t, y_train), axis=0)
+    #         fd_x_t = np.concatenate((fd_x_t, fd_x_train), axis=0)
+    #
+    # X_train, y_train, fd_x_train = X_t, y_t, fd_x_t
+    #
+    # X_t, y_t = None, None
+    # fd_x_t = None
+    # for test_data in task_test:
+    #     testing_path = base_path + f"train_{test_data}_data"
+    #     try:
+    #         f = open(testing_path)
+    #     except IOError:
+    #         create_and_save_training_testing_data(test_data, base_path)
+    #
+    #     _, _, X_test, y_test = read_train_test(test_data, base_path)
+    #     fd_x_test = X_test[:, 0].reshape(-1, 1)
+    #
+    #     if X_t is None:
+    #         X_t = X_test
+    #         y_t = y_test
+    #         fd_x_t = fd_x_test
+    #     else:
+    #         X_t = np.concatenate((X_t, X_test), axis=0)
+    #         y_t = np.concatenate((y_t, y_test), axis=0)
+    #         fd_x_t = np.concatenate((fd_x_t, fd_x_test), axis=0)
+    # X_test, y_test, fd_x_test = X_t, y_t, fd_x_t
+
+    # train fd
+    model = FD_Network()
+    fd_losses = bootstrapped_cv(hidden_dims=None, data=fd_x_train, targets=y_train, test_data=fd_x_test, test_targets=y_test,
+                                kfolds=5, epochs=1000, batch_size=32, n_bootstraps=15, bootstrap_dim=1000, model=model)
+    # train speed nn
+    hidden_dims = (3,)
+    nn_losses = bootstrapped_cv(hidden_dims=hidden_dims, data=X_train, targets=y_train, test_data=X_test, test_targets=y_test,
+                                kfolds=5, epochs=1000, batch_size=32, n_bootstraps=15, bootstrap_dim=1000)
+
+    # once we have the selection stats, train an nn on the whole train to give predictions needed for FD model training
+    nn = create_nn(hidden_dims, dropout=-1)
+    nn.compile(optimizer='sgd', loss='mse')
+    # to stop the computation when model is at its cap
+    callback = EarlyStopping(monitor='loss', patience=10)  # default on val_loss
+    nn.fit(x=X_train, y=y_train, epochs=1000, callbacks=[callback], verbose=0)
+    fd_nn_prediction_speeds = nn.predict(x=X_train)
+
+    # train fd
+    model = FD_Network()
+    fd_prediction_losses = bootstrapped_cv(hidden_dims=None, data=fd_x_train, targets=fd_nn_prediction_speeds, test_data=fd_x_test,
+                                           test_targets=y_test, kfolds=5, epochs=1000, batch_size=32, n_bootstraps=15, bootstrap_dim=1000,
+                                           model=model)
+    return nn_losses, fd_losses, fd_prediction_losses
 
 
 if __name__ == '__main__':
